f5bfa97e71b8c244dbcbe0f6c4013476df96e1c8
David Weatherhead <dlweatherhead@gmail.com>
2022-08-27 22:47:57 +0200

    Script naming improvements

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+	[SerializeField] private GameObject objectToSpawn;
+	[SerializeField] private float spawnRate = 1f;
+	[SerializeField] private bool spawnOnStart = false;
+
+    private void Start()
+    {
+        if(spawnOnStart) {
+			InvokeRepeating("Spawn", 0, spawnRate);
+        }
+    }
+
+    void Spawn () {
+		Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/RigidbodyDestroyer.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class RigidbodyDestroyer : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other) {
+            Destroy(other.gameObject);
+    }
+}
